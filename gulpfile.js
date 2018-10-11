b0632a11f44ea770f040cbd1250f5f13f98aaeec
Alexander Erlandsson <alexander.erlandsson@icloud.com>
2018-10-11 14:55:51 +0200

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,29 @@ var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 var minifycss = require('gulp-minify-css');
 
-function gulpSass() {
+var paths = {
+  sass: {
+    src: './scss/css-fadeshow.scss',
+    dest: './css',
+    watch: './scss/**/*.scss',
+  }
+};
+
+gulp.task('sass', function() {
   return gulp
-    .src('./scss/css-fadeshow.scss')
+    .src(paths.sass.src)
     .pipe(sass())
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
-    .pipe(gulp.dest('./css'));
-}
+    .pipe(gulp.dest(paths.sass.dest));
+});
 
-function gulpWatch() {
-  gulp.watch('./scss/**/*.scss', gulpSass);
-}
+gulp.task('watch:sass', function() {
+  gulp.watch(paths.sass.watch, gulp.series('sass'));
+});
 
-gulp.task('sass', gulpSass);
-gulp.task('watch', gulpWatch);
+gulp.task('watch', gulp.series('sass',
+  gulp.parallel('watch:sass')
+));
+
 gulp.task('default', gulp.parallel('sass'));
